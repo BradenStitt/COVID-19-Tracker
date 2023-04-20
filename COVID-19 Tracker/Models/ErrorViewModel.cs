ab4181c1e769484d477852b42ae8e8b57fc0895c
BradenStitt <stittbraden@gmail.com>
2023-04-20 08:35:03 -0700

    Add project files.

---
+++ COVID-19 Tracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace COVID_19_Tracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
